ebf5f5d1b528d5b6ed76879bf31078803923da14
nazaninsmac <nzn.amabilis@gmail.com>
2025-03-05 00:12:17 +0330

    added queueController initial structure

--- internal/controller/queueController.go
+++ internal/controller/queueController.go
@@ -1,8 +1,27 @@
-// time range queue chnage max bandwidth chnage
-// download dir change
-// maxtry xhange
-// add queue
-// delete queue
-
 package controller
+
+import(
+	"github.com/placeholder14032/download-manager/internal/download"
+	"github.com/placeholder14032/download-manager/internal/queue"
+)
+
+func EditTimeRange(queueID int64, timeRange TimeRange) error {
+
+}
+
+func EditDirectory(queueID int64, newDir string) error {
+
+}
+
+func EditMaxTry(queueID int64, newMaxTrty int64) error {
+
+}
+
+func AddQueue(name string) error {
+
+}
+
+func DeleteQueue(queueID int64, id int64) error {
+
+}
 
