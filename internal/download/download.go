deeec238d82007a7c2c7a9153e929000e0089a38
nazaninsmac <nzn.amabilis@gmail.com>
2025-03-09 22:41:08 +0330

    fixed typo

--- internal/download/download.go
+++ internal/download/download.go
@@ -3,10 +3,11 @@ package download
 type Download struct {
 	ID           int64
 	URL          string
-	FileRath     string
+	FilePath     string
 	Status       State
 	Progress     int64
 	BytesWritten int64
 	RetryCount   int64
 	MaxRetries   int64
 }
+
