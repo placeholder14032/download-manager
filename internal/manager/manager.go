7fd9d99e389c955e73dc014f2c0a239f307b5ddd
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2025-03-09 23:12:35 +0330

    Added AddDownload and PauseDownload capability to Manager

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,14 +1,63 @@
 package manager
 
-import "github.com/placeholder14032/download-manager/internal/queue"
+import (
+	"errors"
+	"sync"
+
+	"github.com/placeholder14032/download-manager/internal/download"
+	"github.com/placeholder14032/download-manager/internal/queue"
+)
 
 type Manager struct {
+	mu sync.Mutex // used to protect the following fields
 	qs []queue.Queue
+	lastUID int64
 }
 
 func loadJson() {
 	// TODO
 }
+
+func (m *Manager) findQueueIndex(queueId int64) int { // maybe can be used to clean up some dublicate code
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	for i := 0; i < len(m.qs); i++ {
+		if m.qs[i].ID == queueId {
+			return i
+		}
+	}
+	return -1
+}
+
+func (m *Manager) AddDownload(queueId int64, url string) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	//
+	for i := 0; i < len(m.qs); i++ {
+		if m.qs[i].ID == queueId {
+			m.qs[i].AddDownload(download.Download {
+				ID: m.lastUID,
+				URL: url,
+			})
+			m.lastUID++
+			return nil
+		}
+	}
+	return errors.New("bad queue id")
+}
+
+func (m *Manager) PauseDownload(queueId int64, dlId int64) error {
+	m.mu.Lock()
+	defer m.mu.Unlock() // make sure to unlock after use
+	//
+	for i := 0; i < len(m.qs); i++ {
+		if m.qs[i].ID == queueId {
+			return m.qs[i].PauseDownload(dlId) // if it errors we return an error too
+		}
+	}
+	return errors.New("bad queue id")
+}
+
 
 func (m *Manager) Start() {
 	// load json
--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,7 +1,10 @@
 package queue
 
 import (
+	"errors"
+	"fmt"
 	"time"
+
 	"github.com/placeholder14032/download-manager/internal/download"
 )
 
@@ -21,3 +24,31 @@ type Queue struct{
 	TimeRange TimeRange
 	MaxRetries int64
 }
+
+func (q *Queue) Init(ID int64) {
+	q.ID = ID
+	q.DownloadLists = make([]download.Download, 0)
+	q.SaveDir = "~/Downloads/" // default. TODO change to sane defaults
+	q.MaxConcurrent = 1
+	q.HasTimeConstraint = false
+	q.TimeRange = TimeRange{time.Time{}, time.Time{}}
+	q.MaxRetries = 1
+}
+
+func (q *Queue) AddDownload(dl download.Download) { // dl passed by value
+	q.DownloadLists = append(q.DownloadLists, dl)
+}
+
+func (q *Queue) PauseDownload(dlId int64) error {
+	for i := 0; i < len(q.DownloadLists); i++ {
+		if q.DownloadLists[i].ID == dlId {
+			if q.DownloadLists[i].Status != download.Downloading {
+				return errors.New(fmt.Sprintf("download with id %d is not currently downloading to be paused", dlId))
+			}
+			q.DownloadLists[i].Status = download.Paused
+			return nil
+		}
+	}
+	return errors.New("bad download id")
+}
+
