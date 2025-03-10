0d3423c8996ab1152ee897f9e19d584f8b9f1460
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2025-03-10 15:09:31 +0330

    created template for handling events in the manager

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -6,16 +6,19 @@ import (
 
 	"github.com/placeholder14032/download-manager/internal/download"
 	"github.com/placeholder14032/download-manager/internal/queue"
+	"github.com/placeholder14032/download-manager/internal/util"
 )
+
+
+
+type Event struct {
+}
 
 type Manager struct {
 	mu sync.Mutex // used to protect the following fields
 	qs []queue.Queue
 	lastUID int64
-}
-
-func loadJson() {
-	// TODO
+	events chan Event
 }
 
 func (m *Manager) findQueueIndex(queueId int64) int { // maybe can be used to clean up some dublicate code
@@ -70,10 +73,34 @@ func (m *Manager) ResumeDownload(queueId int64, dlId int64) error {
 	return errors.New("bad queue id")
 }
 
+func (m *Manager) handleEvent(e Event) {
+}
 
-func (m *Manager) Start() {
-	// load json
+func (m *Manager) answerRequest(r util.Request) {
+}
+
+func (m *Manager) init() {
+}
+
+func (m *Manager) loadJson() {
+	// TODO
+}
+
+func (m *Manager) Start(req chan util.Request, resps chan util.Response) {
 	// start downloading unpaused downloads
-	loadJson()
+	// Initialization
+	m.init()
+	// load json
+	m.loadJson()
+	// starting the main loop handling events and occasionally checking the whole state of things
+	for {
+		select {
+		case e := <- m.events:
+			m.handleEvent(e)
+		case r := <- req:
+			m.answerRequest(r)
+		default:
+		}
+	}
 }
 
