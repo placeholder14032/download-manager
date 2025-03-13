9db7ffd197a804130862a3027923501e524ba6d0
khoshi <khoshneviiis@gmail.com>
2025-03-13 22:59:14 +0330

    added editQueue and selectQueue part

--- cmd/thirdPanel.go
+++ cmd/thirdPanel.go
@@ -1,30 +1,42 @@
+// toDo: chnage the startTime after editing
+// toDo: chnage the endTime after editing
+// toDo: get queues in drawing selectQueueFlex
+
 package cmd
 
 import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+
+	"github.com/placeholder14032/download-manager/internal/queue"
 )
 
-var addQueueFlex, editQueueFlex *tview.Flex
+var selectedQueue queue.Queue
+var selectQueueFlex, selectOptionQueueFlex, addQueueFlex, editQueueFlex *tview.Flex
 var nameQueue, directoryQueue, startTimeQueue, endTimeQueue string
 var maxSimultaneousQueue, maxTryQueue, maxBandwidthQueue int64
 
 func DrawMainQueuePage(app *tview.Application) {
-	var mainFlex = tview.NewFlex()
+	footer := tview.NewTextView().SetText("Press arrow keys to navigate | Enter to confirm | Esc to go back | f[1,2,3] to chnage tabs | Ctrl+q to quit")
+	selectOptionQueueFlex = tview.NewFlex()
 
 	var queueOptions = tview.NewList().
 		AddItem("> NEW QUEUE", "adding new qeueu", 'a', func() { drawNewQueue(app) }).
-		AddItem("> EDIT QUEUE", "edit existing queue", 'b', func() { drawEditQueue(app) })
+		AddItem("> EDIT QUEUE", "edit existing queue", 'b', func() { drawSelectQueue(app) })
 
-	mainFlex = tview.NewFlex().
+	selectOptionQueueFlex = tview.NewFlex().
 		SetDirection(tview.FlexRow).
-		AddItem(queueOptions, 4, 0, true)
+		AddItem(queueOptions, 4, 0, true).
+		AddItem(nil, 0, 1, false).
+		AddItem(footer, 1, 0, false)
 
-	app.SetRoot(mainFlex, true).SetFocus(queueOptions)
+	app.SetRoot(selectOptionQueueFlex, true).SetFocus(queueOptions)
+	StatePanel = "third"
 }
 
 func drawNewQueue(app *tview.Application) {
@@ -37,41 +49,41 @@ func drawNewQueue(app *tview.Application) {
 	startTimeQueueInput := tview.NewInputField().SetLabel("Start Time: ")
 	endTimeQueueInput := tview.NewInputField().SetLabel("End Time: ")
 	maxTryQueueInput := tview.NewInputField().SetLabel("Max Retry: ")
-	var InputFields []*tview.InputField = []*tview.InputField{nameQueueInput, directoryQueueInput, maxSimultaneousQueueInput, maxBandwidthQueueInput, startTimeQueueInput, endTimeQueueInput, maxTryQueueInput}
+	var inputFields []*tview.InputField = []*tview.InputField{nameQueueInput, directoryQueueInput, maxSimultaneousQueueInput, maxBandwidthQueueInput, startTimeQueueInput, endTimeQueueInput, maxTryQueueInput}
 
 	nameQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			nameQueue = nameQueueInput.GetText()
 			currentStep++
-			app.SetFocus(InputFields[currentStep])
+			app.SetFocus(inputFields[currentStep])
 		}
 	})
 	directoryQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			directoryQueue = directoryQueueInput.GetText()
 			currentStep++
-			app.SetFocus(InputFields[currentStep])
+			app.SetFocus(inputFields[currentStep])
 		}
 	})
 	maxSimultaneousQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			maxSimultaneousQueue, _ = strconv.ParseInt(maxSimultaneousQueueInput.GetText(), 10, 64)
 			currentStep++
-			app.SetFocus(InputFields[currentStep])
+			app.SetFocus(inputFields[currentStep])
 		}
 	}).SetAcceptanceFunc(tview.InputFieldInteger)
 	maxBandwidthQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			maxBandwidthQueue, _ = strconv.ParseInt(maxBandwidthQueueInput.GetText(), 10, 64)
 			currentStep++
-			app.SetFocus(InputFields[currentStep])
+			app.SetFocus(inputFields[currentStep])
 		}
 	}).SetAcceptanceFunc(tview.InputFieldInteger)
 	startTimeQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			startTimeQueue = startTimeQueueInput.GetText()
 			currentStep++
-			app.SetFocus(InputFields[currentStep])
+			app.SetFocus(inputFields[currentStep])
 		}
 	}).SetAcceptanceFunc(func(textToCheck string, lastChar rune) bool {
 		if len(textToCheck) == 0 {
@@ -85,7 +97,7 @@ func drawNewQueue(app *tview.Application) {
 		if key == tcell.KeyEnter {
 			endTimeQueue = endTimeQueueInput.GetText()
 			currentStep++
-			app.SetFocus(InputFields[currentStep])
+			app.SetFocus(inputFields[currentStep])
 		}
 	}).SetAcceptanceFunc(func(textToCheck string, lastChar rune) bool {
 		if len(textToCheck) == 0 {
@@ -105,13 +117,13 @@ func drawNewQueue(app *tview.Application) {
 
 	addQueueFlex = tview.NewFlex().
 		SetDirection(tview.FlexRow).
-		AddItem(InputFields[0], 1, 0, true).
-		AddItem(InputFields[1], 1, 0, true).
-		AddItem(InputFields[2], 1, 0, true).
-		AddItem(InputFields[3], 1, 0, true).
-		AddItem(InputFields[4], 1, 0, true).
-		AddItem(InputFields[5], 1, 0, true).
-		AddItem(InputFields[6], 1, 0, true).
+		AddItem(inputFields[0], 1, 0, true).
+		AddItem(inputFields[1], 1, 0, true).
+		AddItem(inputFields[2], 1, 0, true).
+		AddItem(inputFields[3], 1, 0, true).
+		AddItem(inputFields[4], 1, 0, true).
+		AddItem(inputFields[5], 1, 0, true).
+		AddItem(inputFields[6], 1, 0, true).
 		AddItem(nil, 0, 1, false).
 		AddItem(footer, 1, 0, false)
 
@@ -120,21 +132,145 @@ func drawNewQueue(app *tview.Application) {
 		case tcell.KeyUp:
 			if currentStep > 0 {
 				currentStep--
-				app.SetFocus(InputFields[currentStep])
+				app.SetFocus(inputFields[currentStep])
 				return nil
 			}
 		case tcell.KeyDown:
 			if currentStep < maxStep-1 {
 				currentStep++
-				app.SetFocus(InputFields[currentStep])
+				app.SetFocus(inputFields[currentStep])
 				return nil
 			}
 		}
 		return event
 	})
 
-	app.SetRoot(addQueueFlex, true).SetFocus(InputFields[0])
+	app.SetRoot(addQueueFlex, true).SetFocus(inputFields[0])
 }
-func drawEditQueue(app *tview.Application) {
+func drawSelectQueue(app *tview.Application) {
+	// for test
+	var listQueues []queue.Queue = []queue.Queue{queue.Queue{Name: "a", SaveDir: "A", MaxConcurrent: 1, MaxBandwidth: 2, TimeRange: queue.TimeRange{time.Now(), time.Now()}, MaxRetries: 3}}
+	footer := tview.NewTextView().SetText("Press arrow keys to navigate | Enter to confirm | Esc to go back | f[1,2,3] to chnage tabs | Ctrl+q to quit")
 
+	var selectOptions = tview.NewList()
+	for i, q := range listQueues {
+		selectOptions.AddItem(fmt.Sprintf("> %s", q.Name), "", rune('a'+i), func() {
+			selectedQueue = q
+			drawEditQueue(app)
+		})
+	}
+
+	listHeight := len(listQueues)
+
+	selectQueueFlex = tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(selectOptions, 2*listHeight, 0, true).
+		AddItem(nil, 0, 1, false).
+		AddItem(footer, 1, 0, false)
+	app.SetRoot(selectQueueFlex, true).SetFocus(selectOptions)
+}
+
+func getTimeString(t time.Time) string {
+	hour := t.Hour()
+	minute := t.Minute()
+	return (strconv.FormatInt(int64(hour), 10) + ":" + strconv.FormatInt(int64(minute), 10))
+}
+
+func drawEditQueue(app *tview.Application) {
+	footer := tview.NewTextView().SetText("Press arrow keys to navigate | Enter to confirm | Esc to go back | f[1,2,3] to chnage tabs | Ctrl+q to quit")
+	var currentStep, maxStep int = 0, 6
+	directoryQueueInput := tview.NewInputField().SetLabel("Directory: ").SetText(selectedQueue.SaveDir)
+	maxSimultaneousQueueInput := tview.NewInputField().SetLabel("Max Simultaneous: ").SetText(strconv.FormatInt(selectedQueue.MaxConcurrent, 10))
+	maxBandwidthQueueInput := tview.NewInputField().SetLabel("Max Bandwidth: ").SetText(strconv.FormatInt(selectedQueue.MaxBandwidth, 10))
+	startTimeQueueInput := tview.NewInputField().SetLabel("Start Time: ").SetText(getTimeString(selectedQueue.TimeRange.Start))
+	endTimeQueueInput := tview.NewInputField().SetLabel("End Time: ").SetText(getTimeString(selectedQueue.TimeRange.End))
+	maxTryQueueInput := tview.NewInputField().SetLabel("Max Retry: ").SetText(strconv.FormatInt(selectedQueue.MaxRetries, 10))
+	var inputFields []*tview.InputField = []*tview.InputField{directoryQueueInput, maxSimultaneousQueueInput, maxBandwidthQueueInput, startTimeQueueInput, endTimeQueueInput, maxTryQueueInput}
+
+	directoryQueueInput.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			directoryQueue = directoryQueueInput.GetText()
+			currentStep++
+			app.SetFocus(inputFields[currentStep])
+		}
+	})
+	maxSimultaneousQueueInput.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			selectedQueue.MaxConcurrent, _ = strconv.ParseInt(maxBandwidthQueueInput.GetText(), 10, 64)
+			currentStep++
+			app.SetFocus(inputFields[currentStep])
+		}
+	}).SetAcceptanceFunc(tview.InputFieldInteger)
+	maxBandwidthQueueInput.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			selectedQueue.MaxBandwidth, _ = strconv.ParseInt(maxBandwidthQueueInput.GetText(), 10, 64)
+			currentStep++
+			app.SetFocus(inputFields[currentStep])
+		}
+	}).SetAcceptanceFunc(tview.InputFieldInteger)
+	startTimeQueueInput.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			startTimeQueue = startTimeQueueInput.GetText()
+			currentStep++
+			app.SetFocus(inputFields[currentStep])
+		}
+	}).SetAcceptanceFunc(func(textToCheck string, lastChar rune) bool {
+		if len(textToCheck) == 0 {
+			return true
+		}
+		pattern := `^([0-9]|[0-1][0-9]|2[0-3])(:[0-5]?[0-9]?)?$`
+		matched, _ := regexp.MatchString(pattern, textToCheck)
+		return matched
+	})
+	endTimeQueueInput.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			endTimeQueue = endTimeQueueInput.GetText()
+			currentStep++
+			app.SetFocus(inputFields[currentStep])
+		}
+	}).SetAcceptanceFunc(func(textToCheck string, lastChar rune) bool {
+		if len(textToCheck) == 0 {
+			return true
+		}
+		pattern := `^([0-9]|[0-1][0-9]|2[0-3])(:[0-5]?[0-9]?)?$`
+		matched, _ := regexp.MatchString(pattern, textToCheck)
+		return matched
+	})
+	maxTryQueueInput.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			selectedQueue.MaxRetries, _ = strconv.ParseInt(maxTryQueueInput.GetText(), 10, 64)
+			DrawMainQueuePage(app)
+		}
+	}).SetAcceptanceFunc(tview.InputFieldInteger)
+
+	editQueueFlex = tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(inputFields[0], 1, 0, true).
+		AddItem(inputFields[1], 1, 0, true).
+		AddItem(inputFields[2], 1, 0, true).
+		AddItem(inputFields[3], 1, 0, true).
+		AddItem(inputFields[4], 1, 0, true).
+		AddItem(inputFields[5], 1, 0, true).
+		AddItem(nil, 0, 1, false).
+		AddItem(footer, 1, 0, false)
+
+	editQueueFlex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyUp:
+			if currentStep > 0 {
+				currentStep--
+				app.SetFocus(inputFields[currentStep])
+				return nil
+			}
+		case tcell.KeyDown:
+			if currentStep < maxStep-1 {
+				currentStep++
+				app.SetFocus(inputFields[currentStep])
+				return nil
+			}
+		}
+		return event
+	})
+
+	app.SetRoot(editQueueFlex, true).SetFocus(inputFields[0])
 }
