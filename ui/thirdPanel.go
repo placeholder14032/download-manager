92709c9974f47478c0937689f2eb8646467269a6
khoshi <khoshneviiis@gmail.com>
2025-03-21 17:48:26 +0330

    connected third panel to logic w/o QueueName + StringToTime

--- ui/thirdPanel.go
+++ ui/thirdPanel.go
@@ -8,15 +8,17 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 
-	"github.com/placeholder14032/download-manager/internal/queue"
+	"github.com/placeholder14032/download-manager/internal/controller"
+	"github.com/placeholder14032/download-manager/internal/util"
 )
 
-var selectedQueue queue.Queue
+var selectedQueue util.QueueBody
 var selectQueueFlex, selectOptionQueueFlex, addQueueFlex, editQueueFlex *tview.Flex
 var nameQueue, directoryQueue, startTimeQueue, endTimeQueue string
 var maxSimultaneousQueue, maxTryQueue, maxBandwidthQueue int64
@@ -123,8 +125,8 @@ func drawNewQueue(app *tview.Application) {
 	maxTryQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			maxTryQueue, _ = strconv.ParseInt(maxTryQueueInput.GetText(), 10, 64)
-			defer fmt.Println(nameQueue, directoryQueue, maxSimultaneousQueue, maxBandwidthQueue, startTimeQueue, endTimeQueue, maxTryQueue)
-			app.Stop()
+			controller.AddQueue(directoryQueue, nameQueue, maxSimultaneousQueue, maxBandwidthQueue, maxTryQueue, true, strToTimeStart(startTimeQueue), strToTimeEnd(endTimeQueue))
+			DrawMainQueuePage(app)
 		}
 	}).SetAcceptanceFunc(tview.InputFieldInteger)
 
@@ -169,8 +171,7 @@ func drawSelectQueue(app *tview.Application) {
 		SetText("[::b]SELECT QUEUE[::-]").
 		SetDynamicColors(true)
 
-	// for test
-	var listQueues []queue.Queue = []queue.Queue{queue.Queue{Name: "a", SaveDir: "A", MaxConcurrent: 1, MaxBandwidth: 2, TimeRange: queue.TimeRange{time.Now(), time.Now()}, MaxRetries: 3}}
+	listQueues := controller.GetQueues()
 	footer := tview.NewTextView().SetText("Press arrow keys to navigate | Enter to confirm | f[1,2,3] to chnage tabs | Ctrl+q to quit")
 
 	var selectOptions = tview.NewList()
@@ -194,9 +195,10 @@ func drawSelectQueue(app *tview.Application) {
 }
 
 func getTimeString(t time.Time) string {
-	hour := t.Hour()
+	/*hour := t.Hour()
 	minute := t.Minute()
-	return (strconv.FormatInt(int64(hour), 10) + ":" + strconv.FormatInt(int64(minute), 10))
+	return (strconv.FormatInt(int64(hour), 10) + ":" + strconv.FormatInt(int64(minute), 10))*/
+	return t.Format("15:04")
 }
 
 func drawEditQueue(app *tview.Application) {
@@ -208,13 +210,16 @@ func drawEditQueue(app *tview.Application) {
 
 	footer := tview.NewTextView().SetText("Press arrow keys to navigate | Enter to confirm | f[1,2,3] to chnage tabs | Ctrl+q to quit")
 	var currentStep, maxStep int = 0, 6
-	directoryQueueInput := tview.NewInputField().SetLabel("Directory: ").SetText(selectedQueue.SaveDir).SetFieldBackgroundColor(tcell.ColorBlack)
-	maxSimultaneousQueueInput := tview.NewInputField().SetLabel("Max Simultaneous: ").SetText(strconv.FormatInt(selectedQueue.MaxConcurrent, 10)).SetFieldBackgroundColor(tcell.ColorBlack)
-	maxBandwidthQueueInput := tview.NewInputField().SetLabel("Max Bandwidth: ").SetText(strconv.FormatInt(selectedQueue.MaxBandwidth, 10)).SetFieldBackgroundColor(tcell.ColorBlack)
+	directoryQueueInput := tview.NewInputField().SetLabel("Directory: ").SetText(selectedQueue.Directory).SetFieldBackgroundColor(tcell.ColorBlack)
+	maxSimultaneousQueueInput := tview.NewInputField().SetLabel("Max Simultaneous: ").SetText(strconv.FormatInt(selectedQueue.MaxSimul, 10)).SetFieldBackgroundColor(tcell.ColorBlack)
+	maxBandwidthQueueInput := tview.NewInputField().SetLabel("Max Bandwidth: ").SetText(strconv.FormatInt(selectedQueue.MaxBandWidth, 10)).SetFieldBackgroundColor(tcell.ColorBlack)
 	startTimeQueueInput := tview.NewInputField().SetLabel("Start Time: ").SetText(getTimeString(selectedQueue.TimeRange.Start)).SetFieldBackgroundColor(tcell.ColorBlack)
 	endTimeQueueInput := tview.NewInputField().SetLabel("End Time: ").SetText(getTimeString(selectedQueue.TimeRange.End)).SetFieldBackgroundColor(tcell.ColorBlack)
 	maxTryQueueInput := tview.NewInputField().SetLabel("Max Retry: ").SetText(strconv.FormatInt(selectedQueue.MaxRetries, 10)).SetFieldBackgroundColor(tcell.ColorBlack)
 	var inputFields []*tview.InputField = []*tview.InputField{directoryQueueInput, maxSimultaneousQueueInput, maxBandwidthQueueInput, startTimeQueueInput, endTimeQueueInput, maxTryQueueInput}
+
+	startTimeQueue = getTimeString(selectedQueue.TimeRange.Start)
+	endTimeQueue = getTimeString(selectedQueue.TimeRange.End)
 
 	directoryQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
@@ -225,14 +230,15 @@ func drawEditQueue(app *tview.Application) {
 	})
 	maxSimultaneousQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
-			selectedQueue.MaxConcurrent, _ = strconv.ParseInt(maxBandwidthQueueInput.GetText(), 10, 64)
+			selectedQueue.MaxSimul, _ = strconv.ParseInt(maxBandwidthQueueInput.GetText(), 10, 64)
 			currentStep++
 			app.SetFocus(inputFields[currentStep])
 		}
 	}).SetAcceptanceFunc(tview.InputFieldInteger)
+	// submit the edits !!!!!
 	maxBandwidthQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
-			selectedQueue.MaxBandwidth, _ = strconv.ParseInt(maxBandwidthQueueInput.GetText(), 10, 64)
+			selectedQueue.MaxBandWidth, _ = strconv.ParseInt(maxBandwidthQueueInput.GetText(), 10, 64)
 			currentStep++
 			app.SetFocus(inputFields[currentStep])
 		}
@@ -268,6 +274,7 @@ func drawEditQueue(app *tview.Application) {
 	maxTryQueueInput.SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEnter {
 			selectedQueue.MaxRetries, _ = strconv.ParseInt(maxTryQueueInput.GetText(), 10, 64)
+			controller.EditQueue(selectedQueue.ID, selectedQueue.Directory, selectedQueue.Name, selectedQueue.MaxSimul, selectedQueue.MaxBandWidth, selectedQueue.MaxRetries, true, strToTimeStart(startTimeQueue), strToTimeEnd(endTimeQueue))
 			DrawMainQueuePage(app)
 		}
 	}).SetAcceptanceFunc(tview.InputFieldInteger)
@@ -328,4 +335,32 @@ func returnTabHeader() *tview.Flex {
 		AddItem(tab3, 10, 0, false).
 		AddItem(tview.NewTextView().SetBackgroundColor(tcell.ColorBlack), 0, 1, false)
 	return tabHeader
+}
+
+func strToTimeStart(tmpTime string) string {
+	if tmpTime == "" {
+		return "00:00:00"
+	}
+	parts := strings.Split(tmpTime, ":")
+	hours, _ := strconv.Atoi(parts[0])
+
+	minutes := 0
+	if len(parts) > 1 && parts[1] != "" {
+		minutes, _ = strconv.Atoi(parts[1])
+	}
+	return fmt.Sprintf("%02d:%02d:00", hours, minutes)
+}
+
+func strToTimeEnd(tmpTime string) string {
+	if tmpTime == "" {
+		return "23:59:00"
+	}
+	parts := strings.Split(tmpTime, ":")
+	hours, _ := strconv.Atoi(parts[0])
+
+	minutes := 59
+	if len(parts) > 1 && parts[1] != "" {
+		minutes, _ = strconv.Atoi(parts[1])
+	}
+	return fmt.Sprintf("%02d:%02d:00", hours, minutes)
 }
