3a5f3a103b7a2d4974a7b27075d53713329b98e2
khoshi <khoshneviiis@gmail.com>
2025-03-21 18:12:37 +0330

    added the necessary line

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/placeholder14032/download-manager/internal/controller"
 	"github.com/placeholder14032/download-manager/internal/manager"
 	"github.com/placeholder14032/download-manager/internal/util"
 	"github.com/placeholder14032/download-manager/ui"
@@ -9,6 +10,7 @@ import (
 func main() {
 	var reqs = make(chan util.Request)
 	var resps = make(chan util.Response)
+	controller.SetChannels(reqs, resps)
 	var manager = manager.Manager{}
 	go manager.Start(reqs, resps)
 	ui.Main()
